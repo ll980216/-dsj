c746248d8e3c3a9631596e177ee7399399d7bc9c
lily <905355741@qq.com>
2022-08-04 14:52:23 +0800

    修改后

---
+++ assets/js/user_info.js
@@ -0,0 +1,23 @@
+$(function () {
+    const form = layui.form;
+    // 自定义校验规则
+    form.verify({
+        nickname: (val) => {
+            if (val.length > 6) return "昵称长度必须在 1 ~ 6 个字符之间！";
+        },
+    });
+})
+const layer = layui.layer;
+// 初始化用户信息
+const initUserInfo = () => {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        success: (res) => {
+            if (res.status !== 0) return layer.msg("获取用户信息失败！");
+            console.log(res);
+        },
+    });
+};
+
+initUserInfo();
